7b75caf57281f779c966982c8e162392c99413cc
Kevin van der Linde <kevin.vanderlinde@youngcoders.nl>
2018-05-05 12:53:25 +0200

    Add recourcetype

--- src/main/java/nl/crado/game/captclash/game/building/BuildingType.java
+++ src/main/java/nl/crado/game/captclash/game/building/BuildingType.java
@@ -3,21 +3,40 @@ package nl.crado.game.captclash.game.building;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Optional;
+
 @AllArgsConstructor
 public enum BuildingType {
 
-    OFFICE(2, "Office", 10),
-    CAFFE(1, "Cafe",  30)
+    OFFICE(2, "Office", 10, RecourceType.NONE),
+    CAFFE(1, "Cafe",  30, RecourceType.CAFFEINE)
 
-;
+    ;
 
 
     //TODO add all the default building values
     @Getter private final int defaultLevel;
 
-
-    @Getter private String name;
+    //Details about the building
+    @Getter private final String name;
     @Getter private final int maxLevel;
 
+    private final RecourceType type;
+
+    public Optional<RecourceType> getRecourceType() {
+        return (type == RecourceType.NONE ? Optional.empty() : Optional.ofNullable(type));
+    }
+
+
+
+
+    public enum RecourceType {
+        NONE,
+        CAFFEINE,
+        ELECTRICITY
+
+        ;
+
+    }
 
 }
