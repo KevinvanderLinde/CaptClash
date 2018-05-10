683fc89007c3bce361529e8d0923056b2d7ea9ba
Kevin van der Linde <kevin.vanderlinde@youngcoders.nl>
2018-05-10 17:05:22 +0200

    change some js values.

--- src/main/resources/static/assets/javascript/remote.js
+++ src/main/resources/static/assets/javascript/remote.js
@@ -1,14 +1,15 @@
 var users = new Bloodhound({
     datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    prefetch: '../data/films/post_1960.json', // something like a cache that gets loaded during init
     remote: {
-        url: '/suggestion.json?searchTerm=%QUERY',
+        url: '/suggestion?searchstr=%QUERY',
         wildcard: '%QUERY'
     }
 });
 
 $('#remote .typeahead').typeahead(null, {
+    hint: true,
+    minLenght: 1,
     name: 'all-users',
     display: 'value',
     source: users
